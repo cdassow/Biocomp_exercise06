c64bb6cc094fabf288dd48ba811c54c0a82e7536
Grace Petrosini <gpetrosi@nd.edu>
2019-11-01 11:11:35 -0400

    beginning to write pseudo code

---
+++ Part1.R
@@ -0,0 +1,6 @@
+#R version of Unix's head function
+read.csv("wages.csv",
+         sep = ",")
+x <-"""file that you are extracting lines from"""
+n <- """number of lines to be selected"""
+head (x, n=aL)
