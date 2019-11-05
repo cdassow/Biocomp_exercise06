9635ed1a5106c0245317b8d47876b6e4139134de
Stephen Grisoli <sgrisoli@nd.edu>
2019-11-05 16:09:29 -0500

    Finished project

--- exercise_6_number1.R
+++ exercise_6_number1.R
@@ -1,6 +1,12 @@
-#function usage head_function(input_file, number of lines to be printed)
-head_function <- function(input_file, num){
-  writeLines(readLines(input_file,num))
+#function usage head_function(input_file, number of data lines to be printed, booleean of header or not) 
+
+head_function <- function(input_file, num, bool_header){
+  if(bool_header){
+    writeLines(readLines(input_file,num+1))
+  }
+  if(!bool_header){
+    writeLines(readLines(input_file,num))
+  }
 }
 
-head_function("./wages.csv", 4)
+head_function("./wages.csv", 4, TRUE)
