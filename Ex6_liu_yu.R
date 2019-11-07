91c7db673f9ae41417bda1fc6f913a441f22118c
user <user@users-mac-mini.dhcp.nd.edu>
2019-11-06 22:06:26 -0500

    Finish the first problem

---
+++ Ex6_liu_yu.R
@@ -0,0 +1,12 @@
+# Question 1
+
+# set path
+setwd('/Users/user/Desktop/excercise6/Biocomp_exercise06/')
+# import the dataset 'wages'
+wages = read.csv('wages.csv')
+# define the number of top lines you want to present
+n = 5
+# print the first 'n' lines
+head(wages,n)
+
+# Question 2
