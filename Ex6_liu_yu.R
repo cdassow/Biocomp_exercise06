9a1a68b1aa56c166d7068a50fe2c800d04fbd4c6
katnip <katnip@katnipdembp.dhcp.nd.edu>
2019-11-07 22:41:35 -0500

    complete 2nd question

--- Ex6_liu_yu.R
+++ Ex6_liu_yu.R
@@ -10,3 +10,30 @@ n = 5
 head(wages,n)
 
 # Question 2
+
+# load dataset iris
+iris = read.csv('iris.csv')
+
+## print last two rows of the last two column
+# calculate number of columns and rows of iris
+nc = ncol(iris)
+nr = nrow(iris)
+# print last two rows of the last two column
+iris[c(nr-1,nr),c(nc-1,nc)]
+
+## get the number of observations for each species included in the data set
+table(iris$Species)
+
+## get rows with Sepal.Width > 3.5
+iris[which(iris$Sepal.Width>3.5),]
+
+## write the data for the species setosa to a comma-delimited file names ‘setosa.csv’
+# disset the dataset
+setosa = iris[which(iris$Species=='setosa'),]
+write.csv(setosa,'setosa.csv')
+
+## calculate the mean, minimum, and maximum of Petal.Length for observations from virginica
+virginica = iris[which(iris$Species=='virginica'),]
+mean(virginica$Sepal.Length)
+min(virginica$Sepal.Length)
+max(virginica$Sepal.Length)
